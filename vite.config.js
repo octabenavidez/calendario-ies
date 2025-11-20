95db621b0690e941017077ba6d55e112cda572de
octabenavidez <octabenavidezsarmiento@gmail.com>
2025-11-20 03:53:03 -0300

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calendarioies/', // Base path for GitHub Pages
+  base: "/calendario-ies/", // Base path for GitHub Pages
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+});
